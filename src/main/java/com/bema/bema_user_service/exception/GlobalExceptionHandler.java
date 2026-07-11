a63009fa7d38a0b081dfc4a97d3b30c9f8c6d790
Abdul Rasheed Momand <abrashee89@gmail.com>
2026-07-11 23:51:24 +0200

    security: log unhandled user exceptions

--- src/main/java/com/bema/bema_user_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bema/bema_user_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.bema.bema_user_service.exception;
 
 import com.bema.bema_user_service.dto.common.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -13,6 +15,9 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiResponse<Void>> handleNotFound(ResourceNotFoundException ex) {
@@ -49,6 +54,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGeneric(Exception ex) {
+        log.error("Unhandled exception", ex);
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error("An unexpected error occurred"));
     }
